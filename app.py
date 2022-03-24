f0c0639f37528fd3f1e32711bf57c1d0e2b6af06
lyne cha <lynecha@berkeley.edu>
2022-03-23 17:47:14 -0700

    finished part 1 of building mad libs

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/questions")
+def generate_form():
+    """generate the form"""  
+    return render_template("questions.html", mad_lib_words = silly_story.prompts)
+
+
+@app.post("/results")
+def print_story():
+    #print the story
+    story = silly_story.generate(request.form)
+    print(request.form)
+    return render_template("story.html", jinja_story = story)
+
+
+# @app.get("/questions") --> postrequest
+
+#form 
+# there are inputs we need to collect
+# based on those inputs you 
+#get request -> request.args 
+#post request -> request.forms
